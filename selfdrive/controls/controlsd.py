582205d99be7b8e57b5f937bfe4c37df0896977e
multikyd <72329880+multikyd@users.noreply.github.com>
2021-10-17 23:23:24 +0900

    kyd

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -468,7 +468,7 @@ class Controls:
         if self.state == State.enabled:
           if self.events.any(ET.SOFT_DISABLE):
             self.state = State.softDisabling
-            self.soft_disable_timer = 300   # 3s
+            self.soft_disable_timer = 50   # 3s
             self.current_alert_types.append(ET.SOFT_DISABLE)
 
         # SOFT DISABLING
