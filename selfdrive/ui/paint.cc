9c4590a83f00142e152371385a4c9c0e72821b81
Chanho Jung <jungch@naver.com>
2021-10-20 01:05:22 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -420,7 +420,7 @@ static void ui_draw_gear( UIState *s ) {
   const int x = 15*2 + 180;
   const int y = 700;
   const int center_x = x + 90;
-  const int center_y = y + 100;
+  const int center_y = y + 90;
   int ngetGearShifter = int(scene.getGearShifter);
   char str_msg[512];
   char strGear[512]; 
