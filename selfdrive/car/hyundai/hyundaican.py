91b9dc00a9f24d1944a859e9b5fbfe5c98be4ad2
Chanho Jung <jungch@naver.com>
2021-11-09 08:23:47 +0900

    Update hyundaican.py

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -79,7 +79,7 @@ def create_clu11(packer, frame, clu11, button, speed = None, bus = 0):
   values["CF_Clu_AliveCnt1"] = frame % 0x10
   return packer.make_can_msg("CLU11", bus, values)
 
-def create_lfahda_mfc(packer, enabled, hda_set_speed=0):
+def create_lfahda_mfc(packer, enabled, hda_set_speed=2):
   values = {
     "LFA_Icon_State": 2 if enabled else 0,
     "HDA_Active": 1 if hda_set_speed else 0,
